e76a7fcf2bd970d5de2591fdc2d723128cf8194f
Nero007x <79926748+Nero007x@users.noreply.github.com>
2025-05-25 02:31:24 +0530

    add json method

---
+++ javascript/jsonStringfy.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Custom implementation of JSON.stringify
+ * @param value - The value to convert to a JSON string
+ * @returns The JSON string representation of the value
+ */
+declare function jsonStringify(value: unknown): string;
+
+export default jsonStringify; 
